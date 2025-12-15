78067b8c34633042a6753338f7f0691c78a280fa
ratsy-dev <raghavnayakcs88@gmail.com>
2025-12-15 12:33:23 +0530

    fixed map linking to signup, some text in become a merchant and fixed navigating button apply now, and fixed sponsered by autoplay swiper.js

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,26 +1,25 @@
-import React from 'react'
-import Banner from './Banner'
-import HomeCategory from './HomeCategory'
-import Register from './Register'
-import LocationSprade from './LocationSprade'
-import AboutUs from './AboutUs'
-import AppSection from './AppSection'
-import Sponsor from './Sponsor'
-import CategoryShowCase from './CategoryShowCase'
+import React from "react";
+import Banner from "./Banner";
+import HomeCategory from "./HomeCategory";
+import Register from "./Register";
+import LocationSprade from "./LocationSprade";
+import AboutUs from "./AboutUs";
+import AppSection from "./AppSection";
+import Sponsor from "./Sponsor";
+import CategoryShowCase from "./CategoryShowCase";
 
 const Home = () => {
   return (
     <div>
-        <Banner/>
-        <HomeCategory/>
-        <CategoryShowCase/>
-        <Register/>
-        <LocationSprade/>
-        <AboutUs/>
-        <AppSection/>
-        <Sponsor/>
+      <Banner />
+      <HomeCategory />
+      <CategoryShowCase />
+      <Register />
+      <LocationSprade />
+      <AboutUs />
+      <Sponsor />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/pages/Home/Sponsor.jsx
+++ src/pages/Home/Sponsor.jsx
@@ -1,87 +1,75 @@
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
-
+import { Swiper, SwiperSlide } from "swiper/react";
 // Import Swiper styles
-import 'swiper/css';
-
-
-// import required modules
-import { Autoplay} from 'swiper/modules';
-
+import "swiper/css";
+import "swiper/css/autoplay";
+import { Autoplay } from "swiper/modules";
 
 const sponsorList = [
-    {
-        imgUrl: '/src/assets/images/sponsor/01.png',
-      
-    },
-    {
-        imgUrl: '/src/assets/images/sponsor/02.png',
-      
-    },
-    {
-        imgUrl: '/src/assets/images/sponsor/03.png',
-  
-    },
-    {
-        imgUrl: '/src/assets/images/sponsor/04.png',
-
-    },
-    {
-        imgUrl: '/src/assets/images/sponsor/05.png',
-        
-    },
-    {
-        imgUrl: '/src/assets/images/sponsor/06.png',
-        
-    },
-]
-
+  { imgUrl: "/src/assets/images/sponsor/01.png" },
+  { imgUrl: "/src/assets/images/sponsor/02.png" },
+  { imgUrl: "/src/assets/images/sponsor/03.png" },
+  { imgUrl: "/src/assets/images/sponsor/04.png" },
+  { imgUrl: "/src/assets/images/sponsor/05.png" },
+  { imgUrl: "/src/assets/images/sponsor/06.png" },
+];
 
 const Sponsor = () => {
-    return (
-        <div className="sponsor-section section-bg">
-            <div className="container">
-                <div className="section-wrapper">
-                    <div className="sponsor-slider">
-                        <Swiper
-                            spaceBetween={20}
-                            slidesPerView={2}
-                            loop={'true'}
-                            autoplay={{
-                                delay: 2000,
-                                disableOnInteraction: false,
-                            }}
-                            modules={[Autoplay]}
-                            breakpoints={{
-                                0: {
-                                    width: 0,
-                                    slidesPerView: 1,
-                                },
-                                768: {
-                                    width: 768,
-                                    slidesPerView: 3,
-                                },
-                                1200: {
-                                    width: 1200,
-                                    slidesPerView: 5.5,
-                                },
-                            }}
-                        >
-                            {sponsorList.map((val, i) => (
-                                <SwiperSlide key={i}>
-                                    <div className="sponsor-iten">
-                                        <div className="sponsor-thumb">
-                                            <img src={`${val.imgUrl}`} alt={`${val.imgAlt}`} />
-                                        </div>
-                                    </div>
-                                </SwiperSlide>
-                            ))}
-                        </Swiper>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
-}
- 
+  return (
+    <div
+      style={{
+        padding: "6rem 0",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+      className="sponsor-section section-bg"
+    >
+      <h3 style={{ marginBottom: "4rem", fontWeight: "600" }}>Sponsored By</h3>
+
+      <div style={{ width: "100%" }}>
+        <Swiper
+          modules={[Autoplay]}
+          spaceBetween={20}
+          slidesPerView={2}
+          loop={true}
+          autoplay={{
+            delay: 1500,
+            disableOnInteraction: false,
+          }}
+          breakpoints={{
+            0: { slidesPerView: 2 },
+            768: { slidesPerView: 3 },
+            1200: { slidesPerView: 6 },
+          }}
+        >
+          {sponsorList.map((val, i) => (
+            <SwiperSlide key={i}>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  width: "100%",
+                }}
+              >
+                <img
+                  src={val.imgUrl}
+                  alt="sponsor"
+                  style={{
+                    maxWidth: "100%",
+                    maxHeight: "70px",
+                    objectFit: "contain",
+                  }}
+                />
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
+  );
+};
+
 export default Sponsor;
