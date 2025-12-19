a81d5580618d19156e26fc38e71a0275c7852699
ratsy-dev <raghavnayakcs88@gmail.com>
2025-12-20 00:04:54 +0530

    removed app section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,6 @@ import HomeCategory from "./HomeCategory";
 import Register from "./Register";
 import LocationSprade from "./LocationSprade";
 import AboutUs from "./AboutUs";
-import AppSection from "./AppSection";
 import Sponsor from "./Sponsor";
 import CategoryShowCase from "./CategoryShowCase";
 
